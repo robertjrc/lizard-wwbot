30d9c7bd24a00d991630c71b9d358712a560a396
Robert <gh.robert99@gmail.com>
2025-09-12 06:43:50 -0300

    feat(command): search commands implemented.

---
+++ src/commands/search/spotify.js
@@ -0,0 +1,46 @@
+import { MessageMedia } from "../../lib/wwbotjs.js";
+import { axdl } from "../../utils/helpers/axdl.js";
+import { SpofityService } from "../../services/spotifyService.js";
+import { msgResult } from "../../utils/messageResult.js";
+
+export default {
+    name: "spotify",
+    params: ["<name>"],
+    category: "pesquisa",
+    desc: "retorna informações do spotify sobre a música pesquisada.",
+    wait: true,
+    async execute(msg, { args }) {
+        const charLimit = 50;
+        if (!args) {
+            return msg.reply(msgResult("alert", {
+                title: "sem parâmetro",
+                message: "Por favor, forneça o *nome* ou *URL* da música."
+            }));
+        }
+
+        if (args.length > charLimit) {
+            return msg.reply(msgResult("alert", {
+                title: "limite de caracteres",
+                message: `Máximo de *${charLimit}* caracteres.`
+            }));
+        }
+
+        const spotify = new SpofityService();
+        const data = await spotify.getTrack(args);
+
+        const searchProps = {
+            url: data.external_urls.spotify,
+            preview_url: data.preview_url,
+        };
+
+        await msg.reply(searchProps.url, null, { linkPreview: true });
+
+        if (searchProps.preview_url === null) return;
+
+        const audioData = await axdl(searchProps.preview_url);
+        const audioMedia = new MessageMedia('audio/mp3', audioData.toString('base64'), `${Date.now()}.mp3`);
+
+        return await msg.reply(audioMedia);
+    }
+}
+
---
+++ src/commands/search/wiki.js
@@ -0,0 +1,42 @@
+import { WikiService } from "../../services/wikipediaService.js";
+import { msgResult } from "../../utils/messageResult.js";
+
+export default {
+    name: "wiki",
+    params: ["<search>"],
+    category: "pesquisa",
+    wait: true,
+    desc: "Pesquisa um termo na Wikipédia e retorna um resumo com as principais informações encontradas.",
+    async execute(msg, { args }) {
+        let text;
+        let charLimit = 50;
+
+        if (!args) {
+            return msg.reply(msgResult("alert", {
+                title: "sem parâmetro",
+                message: "Nenhuma pesquisa foi passada."
+            }));
+        }
+
+        if (args.length > charLimit) {
+            return msg.reply(msgResult("alert", {
+                title: "limite de caracteres",
+                message: `Máximo de *${charLimit}* caracteres.`
+            }));
+        }
+
+        const response = await WikiService.search(args);
+
+        if (!response.success) {
+            return await msg.reply(msgResult("error", {
+                title: "não foi possível",
+                message: response.message
+            }));
+        }
+
+        text = `${response.data.content}\n\n`
+        text += `${response.data.url}`
+
+        return await msg.reply(text, null, { linkPreview: true });
+    }
+}
