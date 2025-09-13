f713d0688b1dc39359bbfff03b453627b42d9f66
Robert <gh.robert99@gmail.com>
2025-09-13 04:14:23 -0300

    refactor(index): Updated JSON file import.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { Client, LocalAuth } from "./lib/wwbotjs.js";
 import fs from "node:fs";
 import path from "node:path";
+import { importJson } from "./utils/importJson.js";
 (await import("./utils/commandRegistry.js")).loadCommands();
 
-const { ffmpeg_path, executable_path, args } = await (await import("./utils/importJson.js")).importJson("src/config/settings.json");
+const { ffmpeg_path, executable_path, args } = await importJson("src/config/settings.json");
 
 const client = new Client({
     authStrategy: new LocalAuth(),
