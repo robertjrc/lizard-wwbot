ff62d4008d282bc0093719e847d622a1e747bdb3
Robert <gh.robert99@gmail.com>
2025-09-16 15:13:22 -0300

    feat(service) PopCat Service implemented.
    Service to handle PopCat APIs.

---
+++ src/services/popCatService.js
@@ -0,0 +1,159 @@
+import axios from "axios";
+import { URL } from "node:url";
+import { importJson } from "../utils/importJson.js";
+
+const { popcat_api } = await importJson("src/data/URLs.json");
+
+export class PopCatService {
+    #url = new URL(popcat_api);
+
+    async joke() {
+        this.#url.pathname = "/joke";
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: "Não foi possível obter a piada."
+            }
+        }
+
+        return {
+            success: true,
+            data: await this.translate(data.joke, "pt")
+        };
+    }
+
+    async fact() {
+        this.#url.pathname = "/fact";
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: "Não foi possível obter o fato aleatório."
+            }
+        }
+
+        return {
+            success: true,
+            data: await this.translate(data.fact, "pt")
+        };
+    }
+
+    async lyrics(song) {
+        this.#url.pathname = "/lyrics";
+        this.#url.searchParams.append("song", song);
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: await this.translate(data.error, "pt")
+            }
+        }
+
+        return {
+            success: true,
+            data: {
+                lyrics: data.message.lyrics,
+                url: data.message.url
+            }
+        };
+    }
+
+    async npm(packageName) {
+        this.#url.pathname = "/npm"
+        this.#url.searchParams.append("q", packageName);
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: await this.translate(data.error, "pt")
+            }
+        }
+
+        return {
+            success: true,
+            data: {
+                name: data.name,
+                version: data.version,
+                description: await this.translate(data.description, "pt"),
+                author: data.author,
+                last_published: data.last_published,
+                repository: data.repository || null,
+                downloads_this_year: data.downloads_this_year
+            }
+        };
+    }
+
+    async steam(gameName) {
+        this.#url.pathname = "/steam"
+        this.#url.searchParams.append("q", gameName);
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: await this.translate(data.error, "pt")
+            }
+        }
+
+        return {
+            success: true,
+            data: {
+                type: data.type,
+                name: data.name,
+                description: await this.translate(data.description, "pt"),
+                website: data.website,
+                developers: data.developers,
+                publishers: data.publishers,
+                price: data.price
+            }
+        };
+    }
+
+    async periodicTable() {
+        this.#url.pathname = "/periodic-table/random"
+
+        const { data } = await axios.get(this.#url.href);
+
+        if (data.error) {
+            return {
+                success: false,
+                message: await this.translate(data.error, "pt")
+            }
+        }
+
+        return {
+            success: true,
+            data: {
+                name: data.name,
+                symbol: data.symbol,
+                atomic_number: data.atomic_number,
+                atomic_mass: data.atomic_mass,
+                period: data.period,
+                phase: data.phase,
+                discovered_by: data.discovered_by,
+                img_url: data.image,
+                summary: await this.translate(data.summary, "pt")
+            }
+        };
+    }
+
+    async translate(text, lang) {
+        this.#url.pathname = "/translate";
+        this.#url.searchParams.append("to", lang);
+        this.#url.searchParams.append("text", text);
+
+        const { data } = await axios.get(this.#url.href);
+
+        return (data.error) ? "Não foi possível traduzir." : data.translated;
+    }
+}
