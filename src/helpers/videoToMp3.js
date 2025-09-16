48fd4302f893473bd14dab3f740866bf1d527037
Robert <gh.robert99@gmail.com>
2025-09-16 15:27:48 -0300

    feat(helper): video to mp3 implemented.

---
+++ src/helpers/videoToMp3.js
@@ -0,0 +1,36 @@
+import ffmpeg from "fluent-ffmpeg";
+import { Readable } from "stream";
+import { tempFile } from "../utils/tempFile.js";
+import fs from "node:fs/promises";
+import { importJson } from "../utils/importJson.js";
+
+const { ffmpeg_path } = await importJson("src/config/settings.json");
+
+ffmpeg.setFfmpegPath(ffmpeg_path);
+
+export async function videoToMp3(media) {
+    const tempfile = tempFile("mp3");
+    const stream = new Readable();
+    const buffer = Buffer.from(
+        media.data.replace(`data:${media.mimetype};base64,`, ''),
+        'base64'
+    );
+    stream.push(buffer);
+    stream.push(null);
+
+    await new Promise((resolve, reject) => {
+        ffmpeg(stream)
+            .audioCodec('libmp3lame')
+            .toFormat('mp3')
+            .save(tempfile)
+            .on('error', (error) => {
+                reject(error);
+            })
+            .on('end', resolve);
+    });
+
+    const data = await fs.readFile(tempfile, 'base64');
+    await fs.unlink(tempfile);
+
+    return data;
+}
