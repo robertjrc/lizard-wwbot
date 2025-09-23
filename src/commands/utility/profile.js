3ee42bdfdb19d84fdb4d83c104e9b3164bdb1005
Robert <gh.robert99@gmail.com>
2025-09-23 10:27:36 -0300

    style(profile): new output formatting.

--- src/commands/utility/profile.js
+++ src/commands/utility/profile.js
@@ -1,4 +1,5 @@
 import { Member } from "group-analyzer";
+import { importJson } from "../../utils/importJson.js";
 
 export default {
     name: "perfil",
@@ -8,16 +9,20 @@ export default {
         útil para verificar a responsividade da conexão.
     `.replace(/\s+/g, ' ').trim(),
     async execute(msg, { chat }) {
+        const { nickname } = await importJson("src/config/bot.json");
+        const { version } = await importJson("package.json");
         const memberInfo = (await Member.getByGroupId(msg.author, chat.id._serialized)).data;
 
         if (memberInfo.name !== msg._data.notifyName) await Member.newName(msg._data.notifyName);
 
-        let text = `┌─⊣〔 *${memberInfo.shortName}* 〕Lv. *${memberInfo.level}*\n`;
+        let text = `┌──⊣〔 *${nickname}* 〕v${version}\n`;
+        text += "│\n"
+        text += `├ 👤 *${memberInfo.name}*\n`
+        text += `│  ├ Nível: *${memberInfo.level}*\n`;
+        text += `│  ├ Mensagens: *${(memberInfo.messageCount).toLocaleString()}*\n`;
+        text += `│  └ XP: *${memberInfo.xp}*/${memberInfo.xpRequired}\n`;
         text += "│\n";
-        text += `├ 💬 *Mensagens*\n│  └ ${(memberInfo.messageCount).toLocaleString()}\n`;
-        text += `├ ✨ *XP*\n│  └ *${memberInfo.xp}*/${memberInfo.xpRequired}\n`;
-        text += "│\n";
-        text += "└─⊣";
+        text += "└──⊣";
 
         return msg.reply(text);
     }
