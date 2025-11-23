55b945221cf29cda1b8a45443b40afdddcc674b1
Robert <gh.robert99@gmail.com>
2025-11-23 11:14:45 -0300

    feat(getContactLid): NodeChace implemented.

--- src/utils/getContactLid.js
+++ src/utils/getContactLid.js
@@ -1,5 +1,22 @@
+import NodeCache from "node-cache";
+
+const contactsLidCache = new NodeCache();
+
 export async function getContactLid(client, msgAuthor) {
-    return (msgAuthor && msgAuthor.endsWith("@c.us")
-        ? (await client.getContactLidAndPhone(msgAuthor))[0].lid
-        : msgAuthor.author);
+    if(!msgAuthor) return;
+    if (!contactsLidCache.get(msgAuthor)) {
+        if (msgAuthor && msgAuthor.endsWith("@c.us")) {
+            const contactId = (await client.getContactLidAndPhone(msgAuthor))[0].lid;
+
+            contactsLidCache.set(contactId, contactId);
+
+            return contactId;
+        }
+
+        contactsLidCache.set(msgAuthor, msgAuthor);
+
+        return msgAuthor;
+    }
+
+    return contactsLidCache.get(msgAuthor);
 }
