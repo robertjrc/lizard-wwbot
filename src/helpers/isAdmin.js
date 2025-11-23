1ead433dda208e5ae9bba4f44d7aff0627911c24
Robert <gh.robert99@gmail.com>
2025-11-23 11:13:38 -0300

    fix(isAdmin): Support for the new user ID.

--- src/helpers/isAdmin.js
+++ src/helpers/isAdmin.js
@@ -1,3 +1,7 @@
-export function isAdmin(chat, userId) {
-    return (chat.participants.find((user) => user.id._serialized === userId)).isAdmin; 
+export async function isAdmin(client, chat, userId) {
+    const pn = (userId && userId.endsWith("@lid")
+        ? (await client.getContactLidAndPhone(userId))[0].pn
+        : userId);
+
+    return (chat.participants.find((user) => user.id._serialized === pn)).isAdmin;
 }
